f06fb9f4a0068e025f3436ea84fb4d0ad0139757
Abel Pacheco <abelpacheco@outlook.com>
2015-08-02 21:40:06 -0500

    Add data for the last 12hrs for API clients

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,38 @@ class Venue < ActiveRecord::Base
 	has_many :histories, dependent: :destroy
 	validates :name, presence: true
 	# validates :name, presence: true, {message: "Debe escribir un nombre"}
+
+	def histories_simple
+        hist = {}
+        histories.order("created_at DESC").each do |h|   
+        # histories.where(["created_at > ?", 24.hours.ago]).order("created_at DESC").each do |h|   
+          hist[h.created_at] = h.counter
+        end
+        hist
+    end
+
+	def histories12hrs(hrs=12)
+        lasthist = {}
+        [*1..hrs].each do |hr|
+        	x_init   = histories.where(["created_at <= ?", hr.hours.ago]).order("created_at DESC").first
+        	x_init ||= histories.order("created_at ASC").first
+        	x_fin    = histories.where(["created_at >= ?", hr.hours.ago]).order("created_at ASC").first
+        	x_fin  ||= histories.order("created_at DESC").first
+        	x0 = x_init ? x_init.created_at : Time.now
+        	y0 = x_init ? x_init.counter : 0
+        	x1 = x_fin  ? x_fin.created_at : Time.now
+        	y1 = x_fin  ? x_fin.counter : 0
+        	x  = hr.hours.ago
+        	y  = linearinterpolation(x0, y0, x1, y1, x)
+        	lasthist[x] = y
+        end
+        lasthist[0.hours.ago] = histories.order("created_at DESC").first.counter unless histories.blank?
+        lasthist
+    end
+
+    def linearinterpolation(x0, y0, x1, y1, x)
+    	y = y0 + (((y1-y0)*(x-x0))/(x1-x0)) unless x1==x0
+    	y ||= y0
+    end
+
 end
--- app/views/venues/show.json.jbuilder
+++ app/views/venues/show.json.jbuilder
@@ -1,1 +1,8 @@
-json.extract! @venue, :id, :counter, :name, :created_at, :updated_at, :devices
+json.extract! @venue, 
+:id, 
+:counter, 
+:name, 
+:created_at, 
+:updated_at, 
+:devices, 
+:histories12hrs
